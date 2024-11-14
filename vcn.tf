a455dcaf4cc4b1be0f786ad75589a8422b1a6ec1
mayankkakani1 <129320539+mayankkakani1@users.noreply.github.com>
2024-11-14 18:30:50 +0530

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -29,3 +29,10 @@ resource "oci_core_subnet" "example_subnet2" {
     vcn_id = oci_core_vcn.example_vcn.id
     cidr_block = "172.16.1.0/24"
 }
+
+resource "oci_core_subnet" "example_subnet3" {
+    compartment_id = var.compartment_id
+    display_name = "example_subnet3"
+    vcn_id = oci_core_vcn.example_vcn.id
+    cidr_block = "172.16.2.0/24"
+}
