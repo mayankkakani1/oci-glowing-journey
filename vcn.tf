0b2b686d8617cd5f31990de81d32e32eaf557efe
mayankkakani1 <129320539+mayankkakani1@users.noreply.github.com>
2024-11-14 15:05:05 +0530

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -2,21 +2,30 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = ">=4.67.3"
     }
   }
-  required_version = ">= 1.0.0"
+}
+
+provider "oci"{
+    region = var.region
 }
 
 resource "oci_core_vcn" "example_vcn" {
     compartment_id = var.compartment_id
-    display_name = "<vcn-display-name>"
+    display_name = "example_vcn"
     cidr_blocks = ["10.0.0.0/16"]
 }
 
-resource "oci_core_subnet" "example_subnet" {
+resource "oci_core_subnet" "example_subnet1" {
     compartment_id = var.compartment_id
-    display_name = "<subnet-display-name>"
+    display_name = "example_subnet1"
+    vcn_id = oci_core_vcn.example_vcn.id
+    cidr_block = "10.0.0.0/24"
+}
+
+resource "oci_core_subnet" "example_subnet2" {
+    compartment_id = var.compartment_id
+    display_name = "example_subnet2"
     vcn_id = oci_core_vcn.example_vcn.id
     cidr_block = "10.0.0.0/24"
 }
